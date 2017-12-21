09d4c5bcbf855f5f82404f754590fa3cc450b3f3
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-12-21 22:53:50 +0300

    unused projects are removed

--- react_framework/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import HelloWorldApp from './HelloWorldApp';
-import AppWithProps from './AppWithProps';
-import FirstComponent from './FirstComponent';
-import AppWithState from './AppWithState';
-import AppLifeCycle from './AppLifeCycle';
-
-
-AppRegistry.registerComponent('react_framework', () => AppLifeCycle);
